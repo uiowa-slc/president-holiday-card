233249fafa1dfb7328a9347ac6f62acc3db6ba89
Dustin Quam <quamsta@gmail.com>
2021-12-08 15:44:48 -0600

    started cms stuff, not sure if it's going to main

---
+++ mysite/src/CardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace {
+
+
+
+    class CardController extends \SilverStripe\Control\Controller
+    {
+        private static $url_handlers = [
+            'cards//$Action/$ID' => 'show',
+        ];
+
+        private static $allowed_actions = [
+            'show'
+        ];
+
+        public function index(){
+            $this->redirect('/');
+        }
+
+        public function show($request)
+        {
+            print_r($this->getRequest()->param('ID'));
+        }
+    }
+}

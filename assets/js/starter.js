500f7d72f1280b2ac222180864f585c4f012f352
Dustin Quam <quamsta@gmail.com>
2022-10-28 15:36:56 -0500

    inital work

---
+++ assets/js/starter.js
@@ -0,0 +1,51 @@
+// Importing JavaScript
+//
+// You have two choices for including Bootstrap's JS files—the whole thing,
+// or just the bits that you need.
+
+
+// Option 1
+//
+// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
+
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+// Option 2
+//
+// Import just what we need
+
+// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
+// import "../../node_modules/popper.js/dist/popper.min.js";
+
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+
+
+HTMLElement.prototype.swapClasses = function(dosclasses) {
+    //progressContainer.style.display = 'none';
+    //clearTimeout(timer);
+    console.log('hello');
+    var clases = dosclasses.split(/\s*\,\s*/);
+    var entra = clases[0];
+    var sale = clases[1];
+    if (this.classList.contains(sale)) {
+        this.classList.remove(sale);
+        this.classList.add(entra);
+    } else {
+        this.classList.remove(entra);
+        this.classList.add(sale);
+    }
+    return this;
+};
+
+
+// const spotlightEl = document.querySelector("#spotlight");
+
+// function handleMouseMove(event) {
+//     const { clientX, clientY } = event;
+
+//     spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000ee 600px)`;
+// }
+
+// document.addEventListener("mousemove", handleMouseMove)

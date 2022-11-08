85c8bd4a55a770b9eac633ba810e842f909c77b1
Dustin Quam <quamsta@gmail.com>
2022-11-08 16:55:56 -0600

    updated css

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", function() {
+  // const distance = window.scrollY;
   const distance = window.scrollY * -1.7;
-  document.querySelector("#garland").style.transform = `translateY(${distance * 1}px)`
+
+  document.querySelector("#garland__container").style.transform = `translateY(${distance}px)`
   document.querySelector("#back").style.transform = `translateY(${distance * 0.2}px)`
 })
 

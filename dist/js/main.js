4b6b00500c08147b281819a7970759a8f1aabd0f
Dustin Quam <quamsta@gmail.com>
2022-11-14 16:52:48 -0600

    book test

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,14 @@ window.addEventListener("scroll", function() {
   const distance = window.scrollY * -1.7;
 
   document.querySelector("#garland__container").style.transform = `translateY(${distance}px)`
-  document.querySelector("#back").style.transform = `translateY(${distance * 0.2}px)`
+  document.querySelector("#card-back").style.transform = `translateY(${distance * 0.2}px)`
+  // document.querySelector("#card").style.height = `${document.querySelector("#card-front").offsetHeight + document.querySelector("#card-back").offsetHeight + distance * 1.2}px`;
+  // document.querySelector("#body").style.overflowY = `hidden`;
+  // document.querySelector("#body").style.height = `300px`;
+
+  if(distance < -200 ){
+    document.querySelector("#arrow").style.opacity = 0;
+  }
 })
 
 
